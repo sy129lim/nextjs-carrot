8c16133605fbb5f2bce5e4a8571a5634ebe586cb
sy129lim <sylim@plandocs.co.kr>
2025-07-16 16:01:30 +0900

    메인페이지에 이미지 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import { IconLogin } from '@tabler/icons-react';
@@ -8,7 +9,8 @@ export default function Home() {
   return (
     <div className="flex h-screen flex-col items-center">
       <div className="flex flex-1 flex-col items-center justify-center gap-3">
-        <h1 className="text-4xl font-bold">플닥</h1>
+        <h1 className="text-4xl font-bold text-purple-950">플닥</h1>
+        <Image src="/Logo.png" alt="로고 이미지" width={250} height={50} />
         <span>플랜닥스에 어서오세요!</span>
       </div>
       <div className="mb-9">
@@ -24,7 +26,7 @@ export default function Home() {
           <span className="mr-1">이미 계정이 있나요?</span>
           <Link
             href="/login"
-            className="text-purple-800 underline underline-offset-2"
+            className="text-purple-950 underline underline-offset-2"
           >
             로그인{' '}
           </Link>
